748153d02c8ef44ea601fde2600cf4e2b4e4b3a1
Kana Natsuno <dev@whileimautomaton.net>
2014-03-11 21:45:52 +0900

    Copy definitions of deriv from the book

---
+++ sec-2.3.2-v1.scm
@@ -0,0 +1,49 @@
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        (else
+          (error "unknown expression type -- DERIV" exp))))
+
+(define (variable? x)
+  (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1)
+       (variable? v2)
+       (eq? v1 v2)))
+
+(define (make-sum a1 a2)
+  (list '+ a1 a2))
+
+(define (sum? x)
+  (and (pair? x)
+       (eq? (car x) '+)))
+
+(define (addend s)
+  (cadr s))
+
+(define (augend s)
+  (caddr s))
+
+(define (make-product m1 m2)
+  (list '* m1 m2))
+
+(define (product? x)
+  (and (pair? x)
+       (eq? (car x) '*)))
+
+(define (multiplier p)
+  (cadr p))
+
+(define (multiplicand p)
+  (caddr p))
---
+++ sec-2.3.2-v2.scm
@@ -0,0 +1,18 @@
+(load "./sec-2.3.2-v1.scm")
+
+(define (=number? exp num)
+  (and (number? exp)
+       (= exp num)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
