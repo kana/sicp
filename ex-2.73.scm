ba01755020b35ea069391540e952e2d600543a21
Kana Natsuno <dev@whileimautomaton.net>
2014-05-02 23:23:57 +0900

    Answer Exercise 2.73 (b)

--- ex-2.73.scm
+++ ex-2.73.scm
@@ -51,7 +51,51 @@
 ;;; auxiliary code required to install them in the table used by the program
 ;;; above.
 
-; ...
+(define (=number? exp num)
+  (and (number? exp)
+       (= exp num)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (install-sum-package)
+  ;; internal procedures
+  (define (deriv-sum operands var)
+    (make-sum (deriv (addend operands) var)
+              (deriv (augend operands) var)))
+  (define (addend operands)
+    (car operands))
+  (define (augend operands)
+    (cadr operands))
+  ;; interface to the rest of the system
+  (put 'deriv '+ deriv-sum)
+  'done)
+
+(define (install-product-package)
+  ;; internal procedures
+  (define (deriv-product operands var)
+    (make-sum
+      (make-product (multiplier operands)
+                    (deriv (multiplicand operands) var))
+      (make-product (deriv (multiplier operands) var)
+                    (multiplicand operands))))
+  (define (multiplier operands)
+    (car operands))
+  (define (multiplicand operands)
+    (cadr operands))
+  ;; interface to the rest of the system
+  (put 'deriv '* deriv-product)
+  'done)
 
 ;;; c.  Choose any additional differentiation rule that you like, such as the
 ;;; one for exponents (exercise 2.56), and install it in this data-directed
