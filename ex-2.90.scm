cfb98e783183080262fc04b1e8115aea41e5648e
Kana Natsuno <dev@whileimautomaton.net>
2014-08-26 22:47:30 +0900

    Exercise 2.90: Write a plot to answer

---
+++ ex-2.90.scm
@@ -0,0 +1,23 @@
+;;; Exercise 2.90.  Suppose we want to have a polynomial system that is
+;;; efficient for both sparse and dense polynomials. One way to do this is to
+;;; allow both kinds of term-list representations in our system. The situation
+;;; is analogous to the complex-number example of section 2.4, where we allowed
+;;; both rectangular and polar representations. To do this we must distinguish
+;;; different types of term lists and make the operations on term lists
+;;; generic. Redesign the polynomial system to implement this generalization.
+;;; This is a major effort, not a local change.
+
+
+;; Redefine operations for terms and term lists.
+
+; TODO
+
+
+;; Implement the generic operations for the "sparse" representation.
+
+; TODO
+
+
+;; Implement the generic operations for the "dense" representation.
+
+; TODO
