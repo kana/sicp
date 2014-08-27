ae7b236df4cdb67106095de5d6e9dcf297fa7a54
Kana Natsuno <dev@whileimautomaton.net>
2014-08-27 23:19:56 +0900

    Exercise 2.90: Redefine operations

--- ex-2.90.scm
+++ ex-2.90.scm
@@ -10,7 +10,34 @@
 
 ;; Redefine operations for terms and term lists.
 
-; TODO
+; Nothing has changed for procedures to manipulate terms.  Becasue the
+; representation of terms is the same for both representations of term lists.
+(define (make-term order coeff) (list order coeff))
+(define (order term) (car term))
+(define (coeff term) (cadr term))
+
+; Accessors and the predicate for term lists must be generic operations.
+(define (first-term term-list) (apply-generic 'first-term term-list))
+(define (rest-terms term-list) (apply-generic 'rest-terms term-list))
+(define (empty-termlist? term-list) (apply-generic 'empty-termlist? term-list))
+
+; THE-EMPTY-TERMLIST makes a term list, so its result must be tagged as
+; "sparse" or "dense".  But it takes no argument.  For ease of implementation
+; and for compatibility with existing code such as ADD-TERMS, here I assume
+; that THE-EMPTY-TERMLIST always returns a value in the sparse representation.
+(define (the-empty-termlist)
+  ((get 'the-empty-termlist 'sparse-termlist)))
+
+; ADJOIN-TERM takes takes TERM and TERM-LIST.  While TERM-LIST might be formed
+; in one of the two representations, TERM is always formed in the only one
+; representation.  To simplify implementation, I chose not to tag terms.  So
+; that ADJOIN-TERM cannot be implemented as a simple wrapper of APPLY-GENERIC,
+; like FIRST-TERM and others.  Given arguments must be manually "dispatched" to
+; a right procedure.
+(define (adjoin-term term term-list)
+  ((get 'adjoin-term (type-tag term-list))
+   term
+   (contents term-list)))
 
 
 ;; Implement the generic operations for the "sparse" representation.
