d7c0775dd9146eaf076d7b14c2823c4ddeb6056a
Kana Natsuno <dev@whileimautomaton.net>
2014-12-12 23:59:16 +0900

    Exercise 3.48: Write an implementation

--- ex-3.48.scm
+++ ex-3.48.scm
@@ -21,4 +21,41 @@
 ;;; to modify make-account so that each account is created with a number, which
 ;;; can be accessed by sending an appropriate message.)
 
-; TODO
+(define (serialized-exchange account1 account2)
+  (define (do-exchange account1 account2)
+    (let ((serializer1 (account1 'serializer))
+          (serializer2 (account2 'serializer)))
+      ((serializer1 (serializer2 exchange))
+       account1
+       account2)))
+  (if (<= (account1 'id) (account2 'id))
+    (do-exchange account1 account2)
+    (do-exchange account2 account1)))
+
+(define generate-accout-id
+  (let ([id 0]
+        [s (make-serializer)])
+    (s (lambda ()
+         (set! id (+ id 1))
+         id))))
+
+(define (make-account-and-serializer balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((balance-serializer (make-serializer))
+        (id (generate-accout-id)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) withdraw)
+            ((eq? m 'deposit) deposit)
+            ((eq? m 'balance) balance)
+            ((eq? m 'serializer) balance-serializer)
+            ((eq? m 'id) id)
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m))))
+    dispatch))
