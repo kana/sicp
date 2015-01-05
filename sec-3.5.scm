d0be571d58e8ab8071cd4ac1344f12655bd668da
Kana Natsuno <dev@whileimautomaton.net>
2015-01-05 18:39:25 +0900

    Add procedures defined in section 3.5.3

--- sec-3.5.scm
+++ sec-3.5.scm
@@ -81,3 +81,26 @@
 
 (define (scale-stream stream factor)
   (stream-map (lambda (x) (* x factor)) stream))
+
+
+
+
+(define (square x)
+  (* x x))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))           ; Sn-1
+        (s1 (stream-ref s 1))           ; Sn
+        (s2 (stream-ref s 2)))          ; Sn+1
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
+
+(define (make-tableau transform s)
+  (cons-stream s
+               (make-tableau transform
+                             (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car
+              (make-tableau transform s)))
