5858582ac6d920b6f8771e8bc08d62e2c783f79c
Kana Natsuno <dev@whileimautomaton.net>
2015-06-09 20:19:36 +0900

    Exercise 4.11: Use data abstraction on bindings

--- ex-4.11.scm
+++ ex-4.11.scm
@@ -6,19 +6,28 @@
 ;; Only the representation of a frame is changed.  So that only procedures to
 ;; deal with frame have to be redefined.
 
+(define (make-binding var val)
+  (cons var val))
+
+(define (binding-variable binding)
+  (car binding))
+
+(define (binding-value binding)
+  (cdr binding))
+
 ; (define (make-frame variables values)
 ;   (cons variables values))
 (define (make-frame variables values)
-  (list (map cons variables values)))
+  (list (map make-binding variables values)))
 
 ; (define (frame-variables frame) (car frame))
-(define (frame-variables frame) (map car (car frame)))
+(define (frame-variables frame) (map binding-variable (car frame)))
 
 ; (define (frame-values frame) (cdr frame))
-(define (frame-values frame) (map cdr (car frame)))
+(define (frame-values frame) (map binding-value (car frame)))
 
 ; (define (add-binding-to-frame! var val frame)
 ;   (set-car! frame (cons var (car frame)))
 ;   (set-cdr! frame (cons val (cdr frame))))
 (define (add-binding-to-frame! var val frame)
-  (set-car! frame (cons (cons var val) (car frame))))
+  (set-car! frame (cons (make-binding var val) (car frame))))
