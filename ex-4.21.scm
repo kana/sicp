00d0b3682c0dc75269a208da6433e665c6347a74
Kana Natsuno <dev@whileimautomaton.net>
2015-08-11 19:17:02 +0900

    Answer Exercise 4.21 (b)

--- ex-4.21.scm
+++ ex-4.21.scm
@@ -74,4 +74,15 @@
 ;;;        (lambda (ev? od? n)
 ;;;          (if (= n 0) false (ev? <??> <??> <??>)))))
 
-; TODO
+(define (f x)
+  ((lambda (even? odd?)
+     (even? even? odd? x))
+   (lambda (ev? od? n)
+     (if (= n 0) #t (od? ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     (if (= n 0) #f (ev? ev? od? (- n 1))))))
+
+(print (f  0)) ;==> #t
+(print (f  1)) ;==> #f
+(print (f  9)) ;==> #f
+(print (f 10)) ;==> #t
