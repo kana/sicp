d89e4450f07571b5898582f5e2995d3410881fc0
Kana Natsuno <dev@whileimautomaton.net>
2015-09-05 22:24:50 +0900

    Exercise 4.36: Write a template to answer

---
+++ ex-4.36.scm
@@ -0,0 +1,15 @@
+;;; Exercise 4.36.  Exercise 3.69 discussed how to generate the stream of all
+;;; Pythagorean triples, with no upper bound on the size of the integers to be
+;;; searched.
+
+;;; Explain why simply replacing an-integer-between by an-integer-starting-from
+;;; in the procedure in exercise 4.35 is not an adequate way to generate
+;;; arbitrary Pythagorean triples.
+
+; TODO
+
+;;; Write a procedure that actually will accomplish this. (That is, write
+;;; a procedure for which repeatedly typing try-again would in principle
+;;; eventually generate all Pythagorean triples.)
+
+; TODO
