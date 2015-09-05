59d7128328634a63e804c7b8fd05458293c5c5db
Kana Natsuno <dev@whileimautomaton.net>
2015-09-05 22:24:56 +0900

    Exercise 4.36: Answer why new version is not adequate

--- ex-4.36.scm
+++ ex-4.36.scm
@@ -6,7 +6,32 @@
 ;;; in the procedure in exercise 4.35 is not an adequate way to generate
 ;;; arbitrary Pythagorean triples.
 
-; TODO
+; The modified procedure would be:
+;
+;     (define (a-pythagorean-triples low)
+;       (let ((i (an-integer-starting-from low)))
+;         (let ((j (an-integer-starting-from i)))
+;           (let ((k (an-integer-starting-from j)))
+;             (require (= (+ (* i i) (* j j)) (* k k)))
+;             (list i j k)))))
+;
+; And this version can be translated into C-like language as follows:
+;
+;     for (int i = low; ; i++)
+;     {
+;         for (int j = i; ; j++)
+;         {
+;             for (int k = j; ; k++)
+;             {
+;                 if (i * i + j * j != k * k)
+;                     continue;
+;                 emit(i, j, i);
+;             }
+;         }
+;     }
+;
+; Thus, the most inner loop tries to enumerate infinite number of integers.  So
+; that non-first candidates of i and j will never be enumerated.
 
 ;;; Write a procedure that actually will accomplish this. (That is, write
 ;;; a procedure for which repeatedly typing try-again would in principle
