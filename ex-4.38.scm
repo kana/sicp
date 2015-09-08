fb54a767337cf9b68f57564f5ddef8ba6d80330c
Kana Natsuno <dev@whileimautomaton.net>
2015-09-08 23:38:37 +0900

    Answer Exercise 4.38

---
+++ ex-4.38.scm
@@ -0,0 +1,30 @@
+;;; Exercise 4.38.  Modify the multiple-dwelling procedure to omit the
+;;; requirement that Smith and Fletcher do not live on adjacent floors. How
+;;; many solutions are there to this modified puzzle?
+
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+        (cooper (amb 1 2 3 4 5))
+        (fletcher (amb 1 2 3 4 5))
+        (miller (amb 1 2 3 4 5))
+        (smith (amb 1 2 3 4 5)))
+    (require
+     (distinct? (list baker cooper fletcher miller smith)))
+    (require (not (= baker 5)))
+    (require (not (= cooper 1)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    (require (> miller cooper))
+    ; (require (not (= (abs (- smith fletcher)) 1)))
+    (require (not (= (abs (- fletcher cooper)) 1)))
+    (list (list 'baker baker)
+          (list 'cooper cooper)
+          (list 'fletcher fletcher)
+          (list 'miller miller)
+          (list 'smith smith))))
+
+; baker    | 1 | 1 | 1 | 3 | 3 |
+; cooper   | 2 | 2 | 4 | 2 | 4 |
+; fletcher | 4 | 4 | 2 | 4 | 2 |
+; miller   | 3 | 5 | 5 | 5 | 5 |
+; smith    | 5 | 3 | 3 | 1 | 1 |
