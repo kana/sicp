733800129b91326fdbadb7607c7327244bbba2df
Kana Natsuno <dev@whileimautomaton.net>
2015-09-11 00:52:18 +0900

    Exercise 4.38: Make code runnable

--- ex-4.38.scm
+++ ex-4.38.scm
@@ -2,26 +2,41 @@
 ;;; requirement that Smith and Fletcher do not live on adjacent floors. How
 ;;; many solutions are there to this modified puzzle?
 
-(define (multiple-dwelling)
-  (let ((baker (amb 1 2 3 4 5))
-        (cooper (amb 1 2 3 4 5))
-        (fletcher (amb 1 2 3 4 5))
-        (miller (amb 1 2 3 4 5))
-        (smith (amb 1 2 3 4 5)))
-    (require
-     (distinct? (list baker cooper fletcher miller smith)))
-    (require (not (= baker 5)))
-    (require (not (= cooper 1)))
-    (require (not (= fletcher 5)))
-    (require (not (= fletcher 1)))
-    (require (> miller cooper))
-    ; (require (not (= (abs (- smith fletcher)) 1)))
-    (require (not (= (abs (- fletcher cooper)) 1)))
-    (list (list 'baker baker)
-          (list 'cooper cooper)
-          (list 'fletcher fletcher)
-          (list 'miller miller)
-          (list 'smith smith))))
+(load "./sec-4.3.3.scm")
+
+(ambtest '(begin
+
+            (define (distinct? items)
+              (cond ((null? items) true)
+                    ((null? (cdr items)) true)
+                    ((member (car items) (cdr items)) false)
+                    (else (distinct? (cdr items)))))
+
+            (define (multiple-dwelling)
+              (let ((baker (amb 1 2 3 4 5))
+                    (cooper (amb 1 2 3 4 5))
+                    (fletcher (amb 1 2 3 4 5))
+                    (miller (amb 1 2 3 4 5))
+                    (smith (amb 1 2 3 4 5)))
+                (require
+                  (distinct? (list baker cooper fletcher miller smith)))
+                (require (not (= baker 5)))
+                (require (not (= cooper 1)))
+                (require (not (= fletcher 5)))
+                (require (not (= fletcher 1)))
+                (require (> miller cooper))
+                ; (require (not (= (abs (- smith fletcher)) 1)))
+                (require (not (= (abs (- fletcher cooper)) 1)))
+                (list (list 'baker baker)
+                      (list 'cooper cooper)
+                      (list 'fletcher fletcher)
+                      (list 'miller miller)
+                      (list 'smith smith))))
+
+            (let ((answer (multiple-dwelling)))
+              (print answer))
+
+            ))
 
 ; baker    | 1 | 1 | 1 | 3 | 3 |
 ; cooper   | 2 | 2 | 4 | 2 | 4 |
