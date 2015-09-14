2053986941fdba141ecae26c21aee4722474979a
Kana Natsuno <dev@whileimautomaton.net>
2015-09-15 03:08:21 +0900

    Exercise 4.40: Make code runnable

--- ex-4.40.scm
+++ ex-4.40.scm
@@ -14,23 +14,38 @@
 ;;; possibilities that are not already ruled out by previous restrictions.
 ;;; (Hint: This will require a nest of let expressions.)
 
-(define (multiple-dwelling)
-  (let ((baker (amb 1 2 3 4 5)))
-    (require (not (= baker 5)))
-    (let ((cooper (amb 1 2 3 4 5)))
-      (require (not (= cooper 1)))
-      (let ((fletcher (amb 1 2 3 4 5)))
-        (require (not (= fletcher 5)))
-        (require (not (= fletcher 1)))
-        (require (not (= (abs (- fletcher cooper)) 1)))
-        (let ((miller (amb 1 2 3 4 5)))
-          (require (> miller cooper))
-          (let ((smith (amb 1 2 3 4 5)))
-            (require (not (= (abs (- smith fletcher)) 1)))
-            (require
-              (distinct? (list baker cooper fletcher miller smith)))
-            (list (list 'baker baker)
-                  (list 'cooper cooper)
-                  (list 'fletcher fletcher)
-                  (list 'miller miller)
-                  (list 'smith smith))))))))
+(load "./sec-4.3.3.scm")
+
+(ambtest '(begin
+
+            (define (distinct? items)
+              (cond ((null? items) true)
+                    ((null? (cdr items)) true)
+                    ((member (car items) (cdr items)) false)
+                    (else (distinct? (cdr items)))))
+
+            (define (multiple-dwelling)
+              (let ((baker (amb 1 2 3 4 5)))
+                (require (not (= baker 5)))
+                (let ((cooper (amb 1 2 3 4 5)))
+                  (require (not (= cooper 1)))
+                  (let ((fletcher (amb 1 2 3 4 5)))
+                    (require (not (= fletcher 5)))
+                    (require (not (= fletcher 1)))
+                    (require (not (= (abs (- fletcher cooper)) 1)))
+                    (let ((miller (amb 1 2 3 4 5)))
+                      (require (> miller cooper))
+                      (let ((smith (amb 1 2 3 4 5)))
+                        (require (not (= (abs (- smith fletcher)) 1)))
+                        (require
+                          (distinct? (list baker cooper fletcher miller smith)))
+                        (list (list 'baker baker)
+                              (list 'cooper cooper)
+                              (list 'fletcher fletcher)
+                              (list 'miller miller)
+                              (list 'smith smith))))))))
+
+            (let ((answer (multiple-dwelling)))
+              (print answer))
+
+            ))
