ed75b6fd14ac8bfee23bf958024a759165e3b018
Kana Natsuno <dev@whileimautomaton.net>
2015-09-29 22:54:50 +0900

    Answer Exercise 4.55

---
+++ ex-4.55.scm
@@ -0,0 +1,14 @@
+;;; Exercise 4.55.  Give simple queries that retrieve the following information
+;;; from the data base:
+;;;
+;;; a. all people supervised by Ben Bitdiddle;
+;;;
+;;; b. the names and jobs of all people in the accounting division;
+;;;
+;;; c. the names and addresses of all people who live in Slumerville.
+
+(supervisor ?person (Bitdiddle Ben))
+
+(job ?name (accounting . ?type))
+
+(address ?name (Slumerville . ?detail))
