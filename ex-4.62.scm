09d1f198114389b8af933d113621c95c7b3de051
Kana Natsuno <dev@whileimautomaton.net>
2015-10-06 21:15:38 +0900

    Answer Exercise 4.62

---
+++ ex-4.62.scm
@@ -0,0 +1,22 @@
+;;; Exercise 4.62.  Define rules to implement the last-pair operation of
+;;; exercise 2.17, which returns a list containing the last element of
+;;; a nonempty list. Check your rules on queries such as (last-pair (3) ?x),
+;;; (last-pair (1 2 3) ?x), and (last-pair (2 ?x) (3)). Do your rules work
+;;; correctly on queries such as (last-pair ?x (3)) ?
+
+(rule (last-pair ?x ?x)
+      (same ?x (?e . ())))
+(rule (last-pair (?z . ?y) ?x)
+      (last-pair ?y ?x))
+
+; (last-pair (3) ?x)
+; ==> (last-pair (3) (3))
+
+; (last-pair (1 2 3) ?x)
+; ==> (last-pair (1 2 3) (3))
+
+; (last-pair (2 ?x) (3))
+; ==> (last-pair (2 3) (3))
+
+; (last-pair ?x (3))
+; ==> ...?  There are infinite instances.
