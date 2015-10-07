5dfbed36dbb0e86bc0882f99d755898d78468c3f
Kana Natsuno <dev@whileimautomaton.net>
2015-10-07 20:29:53 +0900

    Answer Exercise 4.63

---
+++ ex-4.63.scm
@@ -0,0 +1,31 @@
+;;; Exercise 4.63.  The following data base (see Genesis 4) traces the
+;;; genealogy of the descendants of Ada back to Adam, by way of Cain:
+;;;
+;;;     (son Adam Cain)
+;;;     (son Cain Enoch)
+;;;     (son Enoch Irad)
+;;;     (son Irad Mehujael)
+;;;     (son Mehujael Methushael)
+;;;     (son Methushael Lamech)
+;;;     (wife Lamech Ada)
+;;;     (son Ada Jabal)
+;;;     (son Ada Jubal)
+;;;
+;;; Formulate rules such as ``If S is the son of F, and F is the son of G, then
+;;; S is the grandson of G'' and ``If W is the wife of M, and S is the son of
+;;; W, then S is the son of M'' (which was supposedly more true in biblical
+;;; times than today) that will enable the query system to find the grandson of
+;;; Cain; the sons of Lamech; the grandsons of Methushael. (See exercise 4.69
+;;; for some rules to deduce more complicated relationships.)
+
+(rule (grandson ?g ?s)
+      (and (son ?g ?f)
+           (son ?f ?s)))
+(rule (son ?m ?s)
+      (or (son ?m ?s)
+          (and (wife ?m ?w)
+               (son ?w ?s))))
+
+(grandson Cain ?x)
+(son Lamech ?x)
+(grandson Methushael ?x)
