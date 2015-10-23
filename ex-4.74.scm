67653682b848a1fe4feb47eeac17449fe6afeed6
Kana Natsuno <dev@whileimautomaton.net>
2015-10-24 00:44:55 +0900

    Answer Exercise 4.74 (a)

---
+++ ex-4.74.scm
@@ -0,0 +1,28 @@
+;;; Exercise 4.74.  Alyssa P. Hacker proposes to use a simpler version of
+;;; stream-flatmap in negate, lisp-value, and find-assertions. She observes
+;;; that the procedure that is mapped over the frame stream in these cases
+;;; always produces either the empty stream or a singleton stream, so no
+;;; interleaving is needed when combining these streams.
+;;;
+;;; a. Fill in the missing expressions in Alyssa's program.
+;;;
+;;;     (define (simple-stream-flatmap proc s)
+;;;       (simple-flatten (stream-map proc s)))
+;;;
+;;;     (define (simple-flatten stream)
+;;;       (stream-map <??>
+;;;                   (stream-filter <??> stream)))
+
+(define (simple-stream-flatmap proc s)
+  (simple-flatten (stream-map proc s)))
+
+(define (simple-flatten stream)
+  (stream-map stream-car
+              (stream-filter (lambda (s) (not (stream-null? s))) stream)))
+
+
+;;; b. Does the query system's behavior change if we change it in this way?
+
+; I think there is no change.
+;
+; TODO: Write why.
