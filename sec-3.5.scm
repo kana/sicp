c231ebc36e15b21c61459a0d1ea1a1d147313e2f
Kana Natsuno <dev@whileimautomaton.net>
2015-10-25 23:40:08 +0900

    Copy missing stream-append from the text

--- sec-3.5.scm
+++ sec-3.5.scm
@@ -31,6 +31,12 @@
       (begin (proc (stream-car s))
              (stream-for-each proc (stream-cdr s)))))
 
+(define (stream-append s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (stream-append (stream-cdr s1) s2))))
+
 
 
 
